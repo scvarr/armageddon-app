52e7ccc3dfb609fa9542f9b5116b964b8e3b226e
Kutorov-MS <kutorovms@mail.ru>
2024-11-17 07:47:33 +0300

    lesson 6-2

---
+++ src/button/Button.jsx
@@ -0,0 +1,11 @@
+export function Button() {
+    return <button onClick={click}>Custom Button</button>
+}
+
+function click(){
+    console.log("Click")
+}
+
+function hello() {
+    console.log("Hello")
+}
